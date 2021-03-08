8c4f2892b932821d45ceefd5ced50fd068c7603c
Ivan <reivanstr@gmail.com>
2021-03-08 08:48:45 +0100

    Beta version 1.9.2
    Not write all dev mode settings and reports

--- Lua code/Reactor/LZN reactor/main.lua
+++ Lua code/Reactor/LZN reactor/main.lua
@@ -12,11 +12,7 @@ dvmode = false
 r_signal = true
 tempControl = true
 maxHeat = 100
-<<<<<<< HEAD
 sides = {SOUTH='NORTH',NORTH='SOUTH',EAST='WEST',WEST='EAST'}
-=======
-sides = {'SOUTH'='NORTH','NORTH'='SOUTH','EAST'='WEST','WEST'='EAST'}
->>>>>>> master
 pushSide = "SOUTH" --Сторона выталкивания
 buffSide = "NORTH" --Противоположная сторона от pushSide
 ebal_ya_w_rot_nizky_tps = 0.1 --Время до и после загрузки злн теплоотвода
@@ -99,19 +95,14 @@ function changeType(reactorType)
 	end
 end
 
-
 if dvmode == true then gpu.setResolution(70,30) else gpu.setResolution(35,11) end -- DeveloperResolution
 if #args > 0 then
+	gpu.fill(1,1,160,50," ")
+	for k,v in ipairs(args) do if v=='-dev' then dvmode = true gpu.setResolution(70,30) print('Включен режим разработчика') os.sleep(3) end
 	for pos,parm in ipairs(args) do
 		if dvmode then print(pos, parm) end
 		if parm == "temp" then maxHeat = tonumber(args[pos+1]) print("Предельная темпиратура установлена в значение: "..args[pos+1].."!") os.sleep(3) end
-<<<<<<< HEAD
-		if parm == "-dev" then dvmode = true gpu.setResolution(70,30) print('Включен режим разработчика')end
-		if parm == "side" then pushSide = args[pos+1] buffSide=sides[pushSide] print('Сторона выталкивания: '..pushSide..'\nСторона буфера: '..buffSide) end
-=======
-		if parm == "-dev" then dvmode = true print('Включен режим разработчика')end
-		if parm = "side" then pushSide = args[pos+1] tempSide=sides['pushSide'] end
->>>>>>> master
+		if parm == "side" then pushSide = args[pos+1] buffSide=sides[pushSide] print('Сторона выталкивания: '..pushSide..'\nСторона буфера: '..buffSide) os.sleep(3) end
 		if parm == "type" then 
 			if args[pos+1] == "lithium" then 
 				reactorType=1 
